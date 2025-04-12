a93fbdbaf2561ae836042e9b525904c43af27025
NagaPrasanthMadduluri-20 <maddulurinagaprasanth9391@gmail.com>
2025-04-12 17:48:56 +0530

    added the sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,37 @@
+export const dynamic = "force-static";
+
+export default async function sitemap() {
+  const staticRoutes = [
+    {
+      url: "https://mountainsquareconstruction.com",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://mountainsquareconstruction.com/about",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://mountainsquareconstruction.com/services",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://mountainsquareconstruction.com/projects",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://mountainsquareconstruction.com/contact",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+  ];
+  return [...staticRoutes];
+}
